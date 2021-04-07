e5da6ec801f3827fb27af31e75f27eb570003d18
MichalKulygin <michal.kulygin@gmail.com>
2021-04-07 22:02:38 +0200

    region validation fixed

--- src/main/java/com/mkgn/weatherservice/location/LocationService.java
+++ src/main/java/com/mkgn/weatherservice/location/LocationService.java
@@ -3,6 +3,9 @@ package com.mkgn.weatherservice.location;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+import java.util.function.Predicate;
+
 @Component
 @RequiredArgsConstructor
 class LocationService {
@@ -12,7 +15,7 @@ class LocationService {
     Location createLocation(String city, String region, String country, Double latitude, Double longitude) {
         Location location = new Location();
         location.setCity(city);
-        location.setRegion(region);
+        Optional.ofNullable(region).filter(Predicate.not(String::isBlank)).ifPresent(location::setRegion);
         location.setCountry(country);
         location.setLatitude(latitude);
         location.setLongitude(longitude);

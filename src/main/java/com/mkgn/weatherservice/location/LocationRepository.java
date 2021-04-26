59535de0c26e032473a2b7e9a79a5eaa73fcc5dd
MichalKulygin <michal.kulygin@gmail.com>
2021-04-26 20:35:20 +0200

    WS-19 bug fixed

--- src/main/java/com/mkgn/weatherservice/location/LocationRepository.java
+++ src/main/java/com/mkgn/weatherservice/location/LocationRepository.java
@@ -2,5 +2,5 @@ package com.mkgn.weatherservice.location;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LocationRepository extends JpaRepository<Location, Long> {
+public interface LocationRepository extends JpaRepository<Location, String> {
 }
